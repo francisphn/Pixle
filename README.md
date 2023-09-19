9b319c604a2209043bac652805b23bdcab3fb0c8
Francis Phan <danny.phan2004@gmail.com>
2023-09-19 15:49:11 +1200

    Add Compose Navigation and README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pixle
+
+Pixle is a new and exciting Wordle™ :large_green_square: :large_yellow_square: 
+:black_square: :large_yellow_square: :large_yellow_square:  variant that 
+allows you to take photos of your real-world environment to detect the key 
+objects that you need to find and complete the daily puzzle. 
+Share with your friends to show off your first-try guess (that you definitely didn’t cheat to get). 
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,9 +50,10 @@ android {
 }
 
 dependencies {
+    val version = "2.7.2"
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$version")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
@@ -66,4 +67,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("androidx.navigation:navigation-compose:$version")
 }
