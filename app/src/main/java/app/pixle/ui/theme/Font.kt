22e690fea57fd1e0907a15c2558f4b2277c8ac7f
Vincent ~ <vno16@uclive.ac.nz>
2023-09-19 16:05:27 +1200

    feat: adding fonts

---
+++ app/src/main/java/app/pixle/ui/theme/Font.kt
@@ -0,0 +1,17 @@
+package app.pixle.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import app.pixle.R
+
+
+val Manrope = FontFamily(
+    Font(R.font.manrope_bold, FontWeight.Bold),
+    Font(R.font.manrope_extrabold, FontWeight.ExtraBold),
+    Font(R.font.manrope_extralight, FontWeight.ExtraLight),
+    Font(R.font.manrope_regular, FontWeight.Normal),
+    Font(R.font.manrope_light, FontWeight.Light),
+    Font(R.font.manrope_medium, FontWeight.Medium),
+    Font(R.font.manrope_semibold, FontWeight.SemiBold),
+)
