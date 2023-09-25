1a84236f0ef8d4b2bfc1d9f210f7ab0fd0acfce0
Francis Phan <danny.phan2004@gmail.com>
2023-09-25 22:24:20 +1300

    PIX-19 Add solution repository to insert an object in parallel using launch

---
+++ app/src/main/java/app/pixle/database/repository/SolutionRepository.kt
@@ -0,0 +1,24 @@
+package app.pixle.database.repository
+
+import app.pixle.database.dao.SolutionDao
+import app.pixle.model.entity.solution.Solution
+import app.pixle.model.entity.solution.SolutionWithItems
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+class SolutionRepository(private val solutionDao: SolutionDao) {
+    suspend fun add(solutionWithItems: SolutionWithItems) = coroutineScope {
+        launch (Dispatchers.IO) { solutionDao.insert(solutionWithItems.solution) }
+        launch (Dispatchers.IO) { solutionDao.insert(solutionWithItems.solutionItems) }
+    }
+
+    suspend fun getLatestLazily() : Solution? {
+        return solutionDao.getLatest()
+    }
+
+    suspend fun getLatestEagerly() : SolutionWithItems? {
+        return solutionDao.getLatestSolutionWithItems()
+    }
+}
