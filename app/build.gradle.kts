f0091aa319f6e2ef77b27687944ba9d007c185bf
Vincent ~ <vno16@uclive.ac.nz>
2023-10-07 16:46:09 +1300

    removed unnecessary changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,6 @@ dependencies {
     // Jetpack Compose, Material Design and Android Core
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
