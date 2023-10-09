eb775934e5af8b40f7bd1ded6773e837feb9538a
Francis Phan <danny.phan2004@gmail.com>
2023-10-09 23:58:41 +1300

    Successfully send messages between two devices

--- app/src/main/java/app/pixle/ui/composable/twicedown/TwiceDownSheet.kt
+++ app/src/main/java/app/pixle/ui/composable/twicedown/TwiceDownSheet.kt
@@ -3,6 +3,8 @@ package app.pixle.ui.composable.twicedown
 import android.Manifest
 import android.content.pm.PackageManager
 import android.util.Log
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.core.animateFloatAsState
@@ -20,19 +22,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.VideogameAsset
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -45,6 +53,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -57,14 +66,127 @@ import app.pixle.ui.composable.PhotoItem
 import app.pixle.ui.composable.PolaroidFrame
 import app.pixle.ui.modifier.opacity
 import app.pixle.ui.theme.Manrope
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.AdvertisingOptions
+import com.google.android.gms.nearby.connection.ConnectionInfo
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback
+import com.google.android.gms.nearby.connection.ConnectionResolution
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo
+import com.google.android.gms.nearby.connection.DiscoveryOptions
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback
+import com.google.android.gms.nearby.connection.Payload
+import com.google.android.gms.nearby.connection.PayloadCallback
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate
+import com.google.android.gms.nearby.connection.Strategy
 import kotlinx.coroutines.launch
+import java.util.UUID
+import kotlin.random.Random
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TwiceDownSheet(onDismiss: () -> Unit) {
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
+
+    val advertisingOptions = AdvertisingOptions.Builder()
+        .setStrategy(Strategy.P2P_POINT_TO_POINT)
+        .build()
+
+
+    val randomNumber = Random.nextInt(1, 10000).toString()
+
+    var otherDeviceId by remember { mutableStateOf<String?>(null) }
+    var otherDeviceName by remember { mutableStateOf<String?>(null) }
+
+    val connectionsClient = Nearby.getConnectionsClient(context)
+
+    val messages = remember { mutableStateListOf<String>() }
+
+
+    val payloadCallback = object : PayloadCallback() {
+        override fun onPayloadReceived(id: String, payload: Payload) {
+            Toast.makeText(context, "Receiving a message", LENGTH_SHORT).show()
+
+            payload.asBytes()?.let {
+                messages.add(String(it, Charsets.UTF_8))
+                Toast.makeText(context, "The message is ${String(it, Charsets.UTF_8)}", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        override fun onPayloadTransferUpdate(id: String, update: PayloadTransferUpdate) {
+            Toast.makeText(context, "Sending/receiving", LENGTH_SHORT).show()
+        }
+    }
+
+    val connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
+        override fun onConnectionInitiated(endpointId: String, info: ConnectionInfo) {
+            Toast.makeText(context, "Receiving a connection from ${info.endpointName}", Toast.LENGTH_SHORT).show()
+
+            connectionsClient.acceptConnection(endpointId, payloadCallback)
+        }
+
+        override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+            when (result.status.statusCode) {
+
+                ConnectionsStatusCodes.STATUS_OK -> {
+                    Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show()
+                }
+
+                ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {
+                    Toast.makeText(context, "Rejected", Toast.LENGTH_SHORT).show()
+                }
+
+                ConnectionsStatusCodes.STATUS_ERROR -> {
+                    Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        override fun onDisconnected(endpointId: String) {
+            Toast.makeText(context, "Disconnected", Toast.LENGTH_SHORT).show()
+        }
+    }
     
+    val endpointDiscoveryCallback = object : EndpointDiscoveryCallback() {
+        override fun onEndpointFound(endpointId: String, endpointInfo: DiscoveredEndpointInfo) {
+            Log.d("pixle:debugNearbyConnections", "Endpoint found $endpointId, ${endpointInfo.endpointName}")
+
+            // If endpoint ID of this device is smaller, it should send a request to connect
+            // And vice versa, if the endpoint ID of this device is larger, it should wait to accept a request.
+
+            otherDeviceId = endpointInfo.endpointName
+
+            otherDeviceName = endpointId
+
+            if (randomNumber.toInt() < otherDeviceId!!.toInt()) {
+                Toast.makeText(context, "This device has a smaller name, so connecting", LENGTH_SHORT).show()
+
+                connectionsClient.requestConnection(
+                    "app.pixle",
+                    endpointId,
+                    connectionLifecycleCallback
+                )
+
+                return
+            } else {
+                Toast.makeText(context, "This device has a larger name, so waiting to be connected", LENGTH_SHORT).show()
+            }
+
+
+        }
+
+        override fun onEndpointLost(endpointId: String) {
+            Log.d("pixle:debugNearbyConnections", "Endpoint lost $endpointId")
+        }
+
+    }
+
+    val discoveryOptions = DiscoveryOptions.Builder().setStrategy(Strategy.P2P_POINT_TO_POINT).build()
+
+
+
     val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
 
     val requiredPermissions = listOf(
@@ -91,13 +213,32 @@ fun TwiceDownSheet(onDismiss: () -> Unit) {
     }
 
     LaunchedEffect(Unit, permissionsState) {
-        Log.d("pixle:debug", "User granted all permissions: $permissionsState")
+        Log.d("pixle:debugNearbyConnections", "User granted all permissions: $permissionsState")
 
         // log every permission that has been granted and that has not been granted
         requiredPermissions.forEach {
-            Log.d("pixle:debug", "Permission $it granted: ${ActivityCompat.checkSelfPermission(
+            Log.d("pixle:debugNearbyConnections", "Permission $it granted: ${ActivityCompat.checkSelfPermission(
                 context, it
             ) == PackageManager.PERMISSION_GRANTED}")
+        }
+
+        if (permissionsState) {
+
+
+            Log.d("pixle:debugNearbyConnections", "This device ID is $randomNumber" )
+
+            connectionsClient.startAdvertising(
+                randomNumber,
+                "app.pixle",
+                connectionLifecycleCallback,
+                advertisingOptions
+            )
+
+             connectionsClient.startDiscovery(
+                 "app.pixle",
+                 endpointDiscoveryCallback,
+                 discoveryOptions
+             )
         }
     }
 
@@ -160,7 +301,41 @@ fun TwiceDownSheet(onDismiss: () -> Unit) {
             
             if (permissionsState) {
 
-                Text(text = "You've granted permissions! All your attempts will be forfeit")
+                Text(text = "You've granted permissions! All your attempts will be forfeit once connected.")
+
+                Column {
+                    LazyColumn {
+                        items(messages.toList()) { message ->
+                            Text(text = message)
+                        }
+                    }
+
+                    Row {
+                        val textState = remember { mutableStateOf(TextFieldValue()) }
+                        TextField(
+                            value = textState.value,
+                            onValueChange = { textState.value = it },
+                            modifier = Modifier.weight(1f)
+                        )
+                        Button(onClick = {
+                             otherDeviceId?.let {
+                                 Toast.makeText(context, "Sending ${textState.value.text}", LENGTH_SHORT).show()
+                                 val value = textState.value.text.toByteArray(Charsets.UTF_8)
+                                 val payload = Payload.fromBytes(value)
+
+                                 Log.d("pixle:debug", "other's device id is $otherDeviceId")
+
+                                 connectionsClient.sendPayload(it, payload)
+
+                                 connectionsClient.sendPayload(otherDeviceName!!, payload)
+
+                             }
+                        }) {
+                            Text("Send")
+                        }
+                    }
+                }
+
 
             } else {
                 Text(
